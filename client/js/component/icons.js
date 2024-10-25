bdce832b31d4d4f0f5c0efeb7611e7f03a8eb076
pabloalmunia <pablo.almunia@corusconsulting.com>
2024-10-25 05:33:04 +0200

    Add HTML, SVG, and CSS Component Files
    Introduce new HTML, SVG, and CSS component files to organize the structure, visual elements, and styling in the Graphane-AI client. The HTML file implements the main UI structure, SVGs are separated for icons and animations, and a consistent styling system is added with stylent and style.js files for CSS variables and layout styles.

---
+++ client/js/component/icons.js
@@ -0,0 +1,19 @@
+// Language=SVG
+export const logo      = `<svg viewBox="0 0 60 60" style="width: 32px">
+  <path stroke="#D80000" stroke-width="2" fill="none" d="M24.2,26.9923 L12.2,26.9923 L6.2,16.6 L12.2,6.2077 L24.2,6.2077 L30.2,16.6 Z" transform-origin="18.2 16.6"/>
+  <path stroke="#00D800" stroke-width="2" fill="none" d="M29.5,30 L35.5,40.3923 L47.5,40.3923 L53.5,30 L47.5,19.6077 L35.5,19.6077 Z" transform-origin="41.5 30"/>
+  <path stroke="#0000D8" stroke-width="2" fill="none" d="M24.2,33.0077 L12.2,33.0077 L6.2,43.4 L12.2,53.7923  L24.2,53.7923  L30.2,43.4  Z" transform-origin="18.2 43.4"/>
+</svg>`;
+
+// Language=SVG
+export const animation = `<svg viewBox="0 0 60 60" style="width: 32px">
+  <path stroke="#D80000" stroke-width="2" fill="none" d="M24.2,26.9923 L12.2,26.9923 L6.2,16.6 L12.2,6.2077 L24.2,6.2077 L30.2,16.6 Z" transform-origin="18.2 16.6">
+    <animateTransform attributeName="transform" type="scale" keyTimes="0; 0.25; 0.5; 0.75; 1" values  ="1;    0;   1;   1;  1" dur="2s" repeatCount="indefinite" />
+  </path>
+  <path stroke="#00D800" stroke-width="2" fill="none" d="M29.5,30 L35.5,40.3923 L47.5,40.3923 L53.5,30 L47.5,19.6077 L35.5,19.6077 Z" transform-origin="41.5 30">
+    <animateTransform attributeName="transform" type="scale" keyTimes="0; 0.25; 0.5; 0.75; 1" values  ="1;   1;   0;   1;   1" dur="2s" repeatCount="indefinite" />
+  </path>
+  <path stroke="#0000D8" stroke-width="2" fill="none" d="M24.2,33.0077 L12.2,33.0077 L6.2,43.4 L12.2,53.7923  L24.2,53.7923  L30.2,43.4  Z" transform-origin="18.2 43.4">
+    <animateTransform attributeName="transform" type="scale" keyTimes="0; 0.25; 0.5; 0.75; 1" values  ="1;   1;   1;   0;   1;" dur="2s" repeatCount="indefinite" />
+  </path>
+</svg>`;
---
+++ client/js/component/style.js
@@ -0,0 +1,101 @@
+// language=CSS
+export default `
+  .body {
+    height : 100%;
+    border : 1px solid var(--st-color-4);
+  }
+  
+  .head {
+    height  : 2em;
+    padding : 0.2em 0.6em;
+  }
+  
+  .main {
+    position   : relative;
+    height     : calc(100% - 8em);
+    overflow-y : auto;
+    padding    : 1em;
+  }
+  
+  .boxUser {
+    color            : var(--st-color-9);
+    background-color : var(--st-color-2);
+    border-radius    : 1em;
+    margin-top       : 1em;
+    padding          : 1em;
+  }
+  
+  .boxAssistant {
+    margin-top : 1em;
+    padding    : 1em;
+  }
+  
+  .footer {
+    height      : 5em;
+    display     : flex;
+    align-items : center;
+    margin      : 0.5em;
+    padding     : 0.5em;
+    tab-size    : 4;
+  }
+  
+  .footer * {
+    font-size : 0.9em;
+    margin    : 0 0.5em;
+  }
+  
+  .footer textarea {
+    border           : 0;
+    min-height       : 3.5em;
+    resize           : none;
+    background-color : transparent;
+    padding          : 0;
+  }
+  
+  .footer button {
+    border-radius : 1em;
+  }
+  
+  .body .welcome {
+    position  : absolute;
+    width     : calc(100% - 2em);
+    top       : 50%;
+    transform : translateY(-50%);
+  }
+  
+  .body .welcome .logo {
+    width   : 60px;
+    height  : 60px;
+    margin  : 1em auto 1.5em auto;
+    display : block;
+  }
+  
+  .body .welcome .typing-effect {
+    margin-left  : auto;
+    margin-right : auto;
+    text-align   : center;
+    font-size    : 2em;
+    white-space  : nowrap;
+    overflow     : hidden;
+    width        : 0;
+    height       : 2em;
+    animation    : typing 2.5s steps(40, end) forwards;
+  }
+  
+  @keyframes typing {
+    from {
+      width : 0;
+    }
+    to {
+      width : 100%;
+    }
+  }
+  
+  #assistantSelect {
+    border  : 0;
+    width   : fit-content;
+    margin  : 0;
+    padding : 0;
+    height  : 1.3em;
+  }
+`;
