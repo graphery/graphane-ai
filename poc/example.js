1360e3524a0bbd49123b81e80bc981fe5be904d4
pabloalmunia <pablo.almunia@corusconsulting.com>
2024-10-29 13:09:12 +0100

    Add scripts to generate province charts using OpenAI API
    Added provincias.csv to store data of Spanish provinces. Created JavaScript files to interact with OpenAI API for generating charts based on CSV data and an example image. The scripts use Graphane assistant settings to automate chart suggestions.

---
+++ poc/example.js
@@ -0,0 +1,52 @@
+import filesystem        from "node:fs";
+import { createRequire } from 'node:module';
+import OpenAI            from "openai";
+
+const openai  = new OpenAI();
+const require = createRequire(import.meta.url);
+
+const config = require('../setup/graphane-assistant.json');
+
+const thread = await openai.beta.threads.create();
+const threadId     = thread.id;
+
+const fileStream = filesystem.createReadStream('./example.webp');
+
+const file = await openai.files.create({
+  file: fileStream,
+  purpose: "assistants",
+});
+
+await openai.beta.threads.messages.create(
+  threadId,
+  {
+    "role"    : "user",
+    "content" : [
+      {
+        type : "text",
+        text : `generame un gráfico como la imagen, pero en Graphane`
+      },
+    ],
+    "attachments": [
+      {
+        file_id: file.id,
+        tools: [{type: "code_interpreter"}]
+      }
+    ]
+  });
+
+let run = await openai.beta.threads.runs.createAndPoll(
+  threadId,
+  {
+    assistant_id : config.assistant.id
+  }
+);
+if (run.status === 'completed') {
+  const messages   = await openai.beta.threads.messages.list(
+    run.thread_id
+  );
+  console.log(messages.data[0].content[0].text.value);
+} else {
+  console.log('run.status', run.status);
+}
+
